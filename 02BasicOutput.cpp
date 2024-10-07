d26496adab0c01f38846e69f2378312fd2a0bdc6
CarlosVillaluz072006 <carloscillaluz07@gmail.com>
2024-10-07 08:25:20 +0800

    activity

---
+++ 02BasicOutput.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "OUR LADY OF LOURDES COLLEGE FOUNDATION" << endl;
+	cout << "COLLEGE OF COMPUTER STUDIES" << endl;
+	cout << "BSIT 1 YEAR" << endl;
+	cout << "Carlos B. Villaluz" << endl;
+	cout << "P-4 Brgy, Benit Tulay na Lupa, Labo C.N" << endl;
+	return 0;
+
+}
