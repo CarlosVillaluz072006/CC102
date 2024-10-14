4c1d68a1351848505e4df3f236f1f3405f1ef6d9
Carlos Villaluz <carlosvillaluz07@gmail.com>
2024-10-14 10:24:54 +0800

    Add files via upload
    Hand on MIDterm

---
+++ CC102_SEVENPM SSS point of sale system.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+
+	float sugarPriceUSD=21.14,ricePricePound=56.52,sardinesPricePound=55.82,coffeePriceUSD=5.58,milkPriceUSD=4.01;
+	float USDtoPHP, PoundtoPHP, totalCostPHP;
+	int  sugarQty, riceQty, sardinesQty, coffeeQty, milkQty;
+	
+	
+	cout << "Enter the quality of sugarPriceUSD";
+	cin >> sugarQty;
+	cout << "Enter the quality of ricePricePound";
+	cout << "Enter the quality of sardinesPricePound";
+	cin >> sardinesQty;
+	cout << "Enter the quality of coffeePriceUSD";
+	cin >> coffeeQty;
+	cout << "Enter the quality of milkPriceUSD";
+	cin >> milkQty;
+	cout << "\n";
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+return 0;
+}
